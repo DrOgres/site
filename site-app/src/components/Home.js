9c49e145b3884ba219652cd884b255ed7509cd78
David <74268782+DrOgres@users.noreply.github.com>
2022-12-26 12:22:46 -0600

    compact nav and site home section

--- site-app/src/components/Home.js
+++ site-app/src/components/Home.js
@@ -1,16 +1,36 @@
 export default function Home() {
-    return (
-        <section className='home' id='home'>
+  return (
+    <section className="home" id="home">
+      <div className="content auto-center">
+        <h1 data-splitting className="rotated medalion">
+          Dr.
+          <br />
+          Ogres
+        </h1>
+      </div>
+      <div className="subtitle">
+        <h3>Code - Design - Music - Art</h3>
+      </div>
+      <div className="navigation flex-col">
         
-        <div className='content rotated auto-center'>
-          <h1 data-splitting className='medalion'>
-            Dr.
-            <br />
-            Ogres
-          </h1>
-        </div>
-        <div className="subtitle"><h3>Code - Design - Music - Art</h3></div>
-        <div className="navigation flex-col"><span className="flex-row start items-base gap-sm"><h2 className="site-link shrink">Projects</h2><p className="link-info grow">I've worked on</p></span> <h2>Tutorials About Contact</h2></div>
-      </section>
-    )
+           <p className="link-info">
+          <span className="site-link">Projects</span>
+           <span>{" "}I've worked on</span>
+          </p>
+          <p className="link-info">
+          <span className="site-link">Tutorials</span>
+            <span>{" "}How to do stuff</span>
+          </p>
+          <p className="link-info">
+          <span className="site-link">About</span>
+            <span>{" "}My skills and stuff</span>
+          </p>
+          <p className="link-info">
+          <span className="site-link">Contact</span>
+            <span>{" "}Get in touch</span>
+          </p>
+       
+      </div>
+    </section>
+  );
 }
