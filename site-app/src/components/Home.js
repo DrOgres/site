9441acb7bdff0decc94e140069b482ebade1b680
David <74268782+DrOgres@users.noreply.github.com>
2022-12-26 19:28:31 -0600

    oni mobile positioned

--- site-app/src/components/Home.js
+++ site-app/src/components/Home.js
@@ -1,37 +1,37 @@
 export default function Home() {
   return (
-    <section className="parallax">
-    <div className="home" id="home">
-      <div className="content auto-center">
-        <h1 data-splitting className="rotated medalion">
-          Dr.
-          <br />
-          Ogres
-        </h1>
-      </div>
-      <div className="subtitle">
-        <h3>Code - Design - Music - Art</h3>
-      </div>
-      <div className="navigation flex-col">
-        
-           <p className="link-info">
-          <span className="site-link">Projects</span>
-           <span>{" "}I've worked on</span>
-          </p>
-          <p className="link-info">
-          <span className="site-link">Tutorials</span>
-            <span>{" "}How to do stuff</span>
-          </p>
-          <p className="link-info">
-          <span className="site-link">About</span>
-            <span>{" "}My skills and stuff</span>
-          </p>
-          <p className="link-info">
-          <span className="site-link">Contact</span>
-            <span>{" "}Get in touch</span>
-          </p>
-       
-      </div>
+    <section className="site-header">
+      <div className="mask">
+        <div className="home content" id="home">
+          
+            <h1  className="rotated medalion">
+              Dr.
+              <br />
+              Ogres
+            </h1>
+          
+          <div className="subtitle">
+            <h3>Code - Design - Music - Art</h3>
+          </div>
+          <div className="navigation flex-col">
+            <p className="link-info">
+              <span className="site-link">Projects</span>
+              <span> I've worked on</span>
+            </p>
+            <p className="link-info">
+              <span className="site-link">Tutorials</span>
+              <span> How to do stuff</span>
+            </p>
+            <p className="link-info">
+              <span className="site-link">About</span>
+              <span> My skills and stuff</span>
+            </p>
+            <p className="link-info">
+              <span className="site-link">Contact</span>
+              <span> Get in touch</span>
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   );
