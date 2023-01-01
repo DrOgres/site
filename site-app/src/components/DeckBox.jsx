829bd82f5a9a98e936c793c3815bac5c031b51b0
DrOgres <drogres@gmail.com>
2023-01-01 13:38:11 -0600

    complete up to new burger

--- site-app/src/components/DeckBox.jsx
+++ site-app/src/components/DeckBox.jsx
@@ -39,7 +39,6 @@ const DeckBox = () => {
       .catch(console.error);
   }, []);
 
-
   function handleScroll(direction) {
     let cardIndex = currentCard;
     if (direction === "up") {
@@ -52,14 +51,12 @@ const DeckBox = () => {
         cardIndex--;
         setCurrentCard(cardIndex);
       }
-
     }
     console.log(cardIndex);
     const visibleCard = document.getElementById(cardIndex);
     visibleCard.scrollIntoView({
       behavior: "smooth",
     });
-
   }
 
   return (
@@ -68,20 +65,32 @@ const DeckBox = () => {
         <CardContainer setDeck={setDeck} deck={deck} />
       </div>
       <div className='no-wrap carosel-control'>
-        {currentCard} of {deck && deck.length -1}
+       
         <button
+        className='carosel-control-button'
           onClick={() => {
-            handleScroll('down');
+            handleScroll("down");
           }}
         >
-          back
+          <svg xmlns='http://www.w3.org/2000/svg' width='40px'
+            height='40px' viewBox='0 0 512 512'>
+            <path d='M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160zm352-160l-160 160c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L301.3 256 438.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0z' />
+          </svg>
         </button>
         <button
-          onClick={() => {       
-            handleScroll('up');
+          className='carosel-control-button'
+          onClick={() => {
+            handleScroll("up");
           }}
         >
-          forth
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='40px'
+            height='40px'
+            viewBox='0 0 512 512'
+          >
+            <path d='M470.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 256 265.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160zm-352 160l160-160c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L210.7 256 73.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0z' />
+          </svg>
         </button>
       </div>
     </>
--- site-app/src/components/Header.jsx
+++ site-app/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import MenuContainer from "./MenuContainer";
 
+
 /**
  * @description A simple component to contain the navigation bar for the site should be
  * placed in the app above any changes to the route so that it always renders
@@ -7,33 +8,37 @@ import MenuContainer from "./MenuContainer";
  * @returns An html template for the header section of the site
  */
 
-//TODO add a button to scroll to the top of the page which is only visible when the user has scrolled down the page more
-// than 50% of the height of the page
 const Header = (props) => {
   const article = document.getElementById("article");
 
   return (
     <>
-      <header className="App-header flex-col justify-start">
+      <header className='App-header flex-col justify-start'>
         <MenuContainer />
       </header>
 
-      <div className="top-button">
-        {" "}
+      <div className='top-button'>
         {
           /* visible only if scrillPos > 100  */
           //TODO animate the button to scale in and out
-          //TODO style the button
-          
+
           props.scrollPos > 100 && (
-            <button
-              className="scroll-to-top"
+            <div
+              className='scroll-to-top'
               onClick={() =>
                 article.scrollTo({ top: 0, left: 0, behavior: "smooth" })
               }
             >
-              Scroll to top
-            </button>
+              <svg
+                width='32'
+                height='32'
+                viewBox='0 0 512 512'
+              >
+                
+                < path stroke="#c62852" stroke-width="3" d='M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zm11.3-395.3l112 112c4.6 4.6 5.9 11.5 3.5 17.4s-8.3 9.9-14.8 9.9H304v96c0 17.7-14.3 32-32 32H240c-17.7 0-32-14.3-32-32V256H144c-6.5 0-12.3-3.9-14.8-9.9s-1.1-12.9 3.5-17.4l112-112c6.2-6.2 16.4-6.2 22.6 0z' id="up-arrow" />
+                
+              </svg>
+            </div>
           )
         }
       </div>
